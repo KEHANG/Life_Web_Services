dd27ad36299a9633dc441405d281fb3a5c0e5d21
KEHANG <kehanghan@gmail.com>
2018-09-17 19:23:27 -0400

    [lws-login] create login view for the lws_app

--- lws/routes.py
+++ lws/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 
 from lws import lws_app
+from lws.forms import LoginForm
 
 @lws_app.route('/')
 @lws_app.route('/index')
@@ -17,3 +18,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@lws_app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
