72402c3f59da861fea8258f60454dae3a1c61d87
KEHANG <kehanghan@gmail.com>
2018-09-18 20:43:32 -0400

    [lws-run] add flask shell context variables

--- run.py
+++ run.py
@@ -1,1 +1,6 @@
-from lws import lws_app
+from lws import lws_app, lws_db
+from lws.models import User, Post
+
+@lws_app.shell_context_processor
+def make_shell_context():
+    return {'db': lws_db, 'User': User, 'Post': Post}
