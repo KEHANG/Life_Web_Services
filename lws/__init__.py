aa70bb1716f5a95dfb966ea09aab24e0fd37025e
KEHANG <kehanghan@gmail.com>
2018-09-19 20:00:46 -0400

    [lws] add login manager for lws

--- lws/__init__.py
+++ lws/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from lws.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 lws_app = Flask(__name__)
 lws_app.config.from_object(Config)
 lws_db = SQLAlchemy(lws_app)
 migrate = Migrate(lws_app, lws_db)
+login = LoginManager(lws_app)
+login.login_view = 'login'
 
 from lws import routes, models
