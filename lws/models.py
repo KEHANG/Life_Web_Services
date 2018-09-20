d854ce64c610d000bf6b1f3b80901f82aebe5d95
KEHANG <kehanghan@gmail.com>
2018-09-19 20:02:32 -0400

    [lws-User] add User methods for password setting and checking
    as well as load user

--- lws/models.py
+++ lws/models.py
@@ -1,8 +1,11 @@
+from lws import login
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from lws import lws_db
 
-class User(lws_db.Model):
+class User(UserMixin, lws_db.Model):
     id = lws_db.Column(lws_db.Integer, primary_key=True)
     username = lws_db.Column(lws_db.String(64), index=True, unique=True)
     email = lws_db.Column(lws_db.String(120), index=True, unique=True)
@@ -13,6 +16,12 @@ class User(lws_db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Post(lws_db.Model):
     id = lws_db.Column(lws_db.Integer, primary_key=True)
     body = lws_db.Column(lws_db.String(140))
@@ -21,3 +30,10 @@ class Post(lws_db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+    
