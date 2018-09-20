f85e85af648b410f2e008b2e60b6398a140aba4d
KEHANG <kehanghan@gmail.com>
2018-09-20 07:02:26 -0400

    [lws-user] add a view function to show user profile

--- lws/models.py
+++ lws/models.py
@@ -1,8 +1,9 @@
-from lws import login
+from hashlib import md5
 from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from lws import login
 from lws import lws_db
 
 class User(UserMixin, lws_db.Model):
@@ -22,6 +23,11 @@ class User(UserMixin, lws_db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
 class Post(lws_db.Model):
     id = lws_db.Column(lws_db.Integer, primary_key=True)
     body = lws_db.Column(lws_db.String(140))
@@ -36,4 +42,3 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-    
