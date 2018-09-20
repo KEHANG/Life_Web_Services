44a5bbdd0ef38c30481bfd82f5b6a1c83195d52d
KEHANG <kehanghan@gmail.com>
2018-09-19 20:45:18 -0400

    [lws-register] add registration view

--- lws/routes.py
+++ lws/routes.py
@@ -2,8 +2,8 @@ from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 
-from lws import lws_app
-from lws.forms import LoginForm
+from lws import lws_app, lws_db
+from lws.forms import LoginForm, RegistrationForm
 from lws.models import User, Post
 
 @lws_app.route('/')
@@ -35,3 +35,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@lws_app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        lws_db.session.add(user)
+        lws_db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
