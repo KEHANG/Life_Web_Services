2c3ab2712fdd3f708117d8879ed448db8f99cb05
KEHANG <kehanghan@gmail.com>
2018-09-22 18:20:29 -0400

    [lws-followed_posts] allow user to get followed posts

--- lws/models.py
+++ lws/models.py
@@ -54,6 +54,13 @@ class User(UserMixin, lws_db.Model):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
 
 class Post(lws_db.Model):
     id = lws_db.Column(lws_db.Integer, primary_key=True)
