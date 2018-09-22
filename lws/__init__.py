9ed95a40dac4cf1d51b7d7b3cd84537289b2e37e
KEHANG <kehanghan@gmail.com>
2018-09-22 17:18:32 -0400

    [lws-mail] mail service setup

--- lws/__init__.py
+++ lws/__init__.py
@@ -1,8 +1,11 @@
+import os
+import logging
 from flask import Flask
 from lws.config import Config
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 lws_app = Flask(__name__)
 lws_app.config.from_object(Config)
@@ -12,3 +15,31 @@ login = LoginManager(lws_app)
 login.login_view = 'login'
 
 from lws import routes, models, errors
+
+if not lws_app.debug:
+    if lws_app.config['MAIL_SERVER']:
+        auth = None
+        if lws_app.config['MAIL_USERNAME'] or lws_app.config['MAIL_PASSWORD']:
+            auth = (lws_app.config['MAIL_USERNAME'], lws_app.config['MAIL_PASSWORD'])
+        secure = None
+        if lws_app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(lws_app.config['MAIL_SERVER'], lws_app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + lws_app.config['MAIL_SERVER'],
+            toaddrs=lws_app.config['ADMINS'], subject='Life Web Services Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        lws_app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/lws.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    lws_app.logger.addHandler(file_handler)
+
+    lws_app.logger.setLevel(logging.INFO)
+    lws_app.logger.info('Life Web Services startup')
