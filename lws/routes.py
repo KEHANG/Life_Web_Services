0638e0e10345c99f97692f8a217d13a94427d1e9
KEHANG <kehanghan@gmail.com>
2018-09-22 17:16:21 -0400

    [lws-edit_profile] add edit_profile page

--- lws/routes.py
+++ lws/routes.py
@@ -4,8 +4,8 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 
 from lws import lws_app, lws_db
-from lws.forms import LoginForm, RegistrationForm
 from lws.models import User, Post
+from lws.forms import LoginForm, RegistrationForm, EditProfileForm
 
 @lws_app.route('/')
 @lws_app.route('/index')
@@ -60,6 +60,23 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@lws_app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(current_user.username)
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        lws_db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
+
 @lws_app.before_request
 def before_request():
     if current_user.is_authenticated:
