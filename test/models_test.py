3ac8f5ecdbed689c51f25b2f969598b6132e180c
KEHANG <kehanghan@gmail.com>
2018-09-22 18:32:18 -0400

    [lws-tests] no need to have the unittest.main()
    since we use nosetests to run all the unittests

--- test/models_test.py
+++ test/models_test.py
@@ -85,6 +85,3 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(f2, [p2, p3])
         self.assertEqual(f3, [p3, p4])
         self.assertEqual(f4, [p4])
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
