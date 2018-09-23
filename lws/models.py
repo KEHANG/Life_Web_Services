c6f2072df02cdda05d98b9cdf1dc64a0e155a7de
KEHANG <kehanghan@gmail.com>
2018-09-23 11:29:12 -0400

    [lws-reset-pswd] after user gets the email, we allow pssword to be reset

--- lws/models.py
+++ lws/models.py
@@ -68,6 +68,15 @@ class User(UserMixin, lws_db.Model):
             {'reset_password': self.id, 'exp': time() + expires_in},
             lws_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, lws_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 
 class Post(lws_db.Model):
     id = lws_db.Column(lws_db.Integer, primary_key=True)
