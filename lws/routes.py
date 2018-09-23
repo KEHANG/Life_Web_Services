26d84fa2a2903053bd1e7a9d6149b24544ffb002
KEHANG <kehanghan@gmail.com>
2018-09-23 09:07:39 -0400

    [lws-pagination] add pagination for post to user profile and home pages

--- lws/routes.py
+++ lws/routes.py
@@ -18,8 +18,20 @@ def index():
         lws_db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = Post.query.all()
-    return render_template('index.html', title='Home', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page, 
+            lws_app.config['POSTS_PER_PAGE'], False)
+
+    next_url = None
+    if posts.has_next:
+        next_url = url_for('index', page=posts.next_num)
+    prev_url = None
+    if posts.has_prev:
+        prev_url = url_for('index', page=posts.prev_num)
+
+    return render_template('index.html', title='Home', form=form, 
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 @lws_app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -62,11 +74,17 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, 
+            lws_app.config['POSTS_PER_PAGE'], False)
+    next_url = None
+    if posts.has_next:
+        next_url = url_for('user', username=user.username, page=posts.next_num)
+    prev_url = None
+    if posts.has_prev:
+        prev_url = url_for('user', username=user.username, page=posts.prev_num)
+    return render_template('user.html', user=user, 
+                           posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @lws_app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
