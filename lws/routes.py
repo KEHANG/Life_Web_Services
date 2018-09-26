164b62d6ffc2dc5e5138bde64d708e7ba1cbb42c
KEHANG <kehanghan@gmail.com>
2018-09-26 17:32:44 -0400

    [lws-translation] post time can also use local language via moment.js

--- lws/routes.py
+++ lws/routes.py
@@ -1,6 +1,7 @@
-from flask_babel import _
+from flask import g
 from datetime import datetime
 from werkzeug.urls import url_parse
+from flask_babel import _, get_locale
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 
@@ -189,3 +190,4 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         lws_db.session.commit()
 
+    g.locale = str(get_locale())
