80a085b9cd2c254161406bdecdad2e16c05f8ff8
KEHANG <kehanghan@gmail.com>
2018-10-27 20:46:59 -0400

    [lws-dish] design dish form with various number of ingredients
    this is done via FieldList and a add_entry button.

--- lws/cook/forms.py
+++ lws/cook/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, FieldList
 from wtforms.validators import DataRequired, ValidationError
 
-from lws.models import Ingredient
+from lws.models import Dish, Ingredient
 
 class IngredientForm(FlaskForm):
     ingredient_name = StringField('Ingredient', validators=[DataRequired()])
@@ -12,3 +12,14 @@ class IngredientForm(FlaskForm):
         ingredient = Ingredient.query.filter_by(name=ingredient_name.data).first()
         if ingredient is not None:
             raise ValidationError('{0} is already in the system.'.format(ingredient_name.data))
+
+class DishForm(FlaskForm):
+    dish_name = StringField('Dish', validators=[DataRequired()])
+    ingredients = FieldList(StringField('Ingredient', validators=[DataRequired()]), min_entries=1)
+    add_entry = SubmitField('One more ingredient')
+    submit = SubmitField('Submit')
+
+    def validate_dish_name(self, dish_name):
+        dish = Dish.query.filter_by(name=dish_name.data).first()
+        if dish is not None:
+            raise ValidationError('{0} is already in the system.'.format(dish_name.data))
