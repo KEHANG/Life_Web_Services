49e3922bbd91071b695355f18a9e78204b0d4c8a
KEHANG <kehanghan@gmail.com>
2020-11-28 23:40:38 -0800

    made StockShare table available for flash shell

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from lws import cli, create_app, lws_db
-from lws.models import User, Post
+from lws.models import User, Post, StockShare
 
 lws_app = create_app()
 cli.register(lws_app)
 
 @lws_app.shell_context_processor
 def make_shell_context():
-    return {'db': lws_db, 'User': User, 'Post': Post}
+    return {'db': lws_db, 
+    		'User': User, 'Post': Post,
+    		'StockShare': StockShare}
